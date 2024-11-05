3cd7a97336a15e9d26b96b3c13bf553966a81eb8
Mr. Awais <161300531+Awais68@users.noreply.github.com>
2024-11-05 21:05:28 +0500

    what did you do?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import HeroSection from "@/components/HeroSection";
 import Header from "@/components/Header";
-
+import DoctorSection from "@/components/DoctorSection";
 export default function Home() {
   return (
     <div className="min-h-screen">
       <Header />
       <HeroSection />
+      <DoctorSection />
       {/* <DoctorsSection isHome={true} /> */}
     </div>
   );
